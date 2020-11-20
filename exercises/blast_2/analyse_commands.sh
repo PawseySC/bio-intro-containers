1fb8b213388762c334990cfe89d537d31060fa86
Marco De La Pierre <marco.delapierre@gmail.com>
2020-11-20 23:06:12 +0800

    edits to exercise files

--- exercises/blast_2/analyse_commands.sh
+++ exercises/blast_2/analyse_commands.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # use docker://quay.io/biocontainers/blast:2.9.0--pl526h3066fca_4
 
-cd ../blast_db
+cd ../database_blast
+gunzip zebrafish.1.protein.faa.gz
 makeblastdb -in zebrafish.1.protein.faa -dbtype prot
 cd -
 
-blastp -query P04156.fasta -db $TUTO/exercises/blast_db/zebrafish.1.protein.faa -out results.txt
+blastp -query P04156.fasta -db ../database_blast/zebrafish.1.protein.faa -out results.txt
--- exercises/pipeline/data/solutions/pipe.2.sh
+++ exercises/pipeline/data/solutions/pipe.2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-salmon_image="$WORK/exercises/pipeline/data/salmon_1.2.1--hf69c8f4_0.sif"
-fastqc_image="$WORK/exercises/pipeline/data/fastqc_0.11.9--0.sif"
-multiqc_image="$WORK/exercises/pipeline/data/multiqc_1.9--pyh9f0ad1d_0.sif"
+salmon_image="/data/abacbs-containers/exercises/pipeline/data/salmon_1.2.1--hf69c8f4_0.sif"
+fastqc_image="/data/abacbs-containers/exercises/pipeline/data/fastqc_0.11.9--0.sif"
+multiqc_image="/data/abacbs-containers/exercises/pipeline/data/multiqc_1.9--pyh9f0ad1d_0.sif"
 
-export SINGULARITY_BINDPATH="$WORK/exercises/pipeline"
+export SINGULARITY_BINDPATH="/data"
 
 
 echo "Pipeline started..."
